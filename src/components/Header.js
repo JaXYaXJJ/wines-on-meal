a6777f8210c5cf370092ab52af31ab6abe67fc42
JaXYaXJJ <ja-ya@mail.ru>
2022-08-19 17:56:29 +0300

    add header.js component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header(props) {
+    return (
+      <nav className="navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand text-light mx-5" href="#">
+            <i className="bi bi-droplet-half"></i>
+            {props.name}
+          </a>
+        </div>
+      </nav>
+    );
+  }
+  
+  export default Header;
